9f0b63a24854229ecbfdd81a3f320de29bfc531f
Luis Montealegre <montealegreluis@gmail.com>
2017-07-15 21:31:58 -0500

    refactor: Use Yelp class to authenticate in class App
    * Use the new Yelp class to get the access token
    * Reorganize blocks of code to search for restaurants

--- src/main/java/com/montealegreluis/yelpv3/App.java
+++ src/main/java/com/montealegreluis/yelpv3/App.java
@@ -1,16 +1,12 @@
 package com.montealegreluis.yelpv3;
 
 import org.apache.http.HttpEntity;
-import org.apache.http.NameValuePair;
 import org.apache.http.StatusLine;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.utils.URIBuilder;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -20,8 +16,6 @@ import java.io.IOException;
 import java.net.URISyntaxException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Properties;
 
 public class App
@@ -31,34 +25,20 @@ public class App
         Path path = Paths.get("src/main/resources/application.properties");
         properties.load(new FileInputStream(path.toAbsolutePath().toString()));
 
-        URIBuilder builder = new URIBuilder();
-        builder.setScheme("https").setHost("api.yelp.com").setPath("/oauth2/token");
-        HttpPost post = new HttpPost(builder.build());
+        Yelp yelp = new Yelp(
+            properties.getProperty("yelp.api.client_id"),
+            properties.getProperty("yelp.api.client_secret")
+        );
+        AccessToken token = yelp.authenticate();
 
-        List<NameValuePair> params = new ArrayList<>();
-        params.add(new BasicNameValuePair("grant_type", "client_credentials"));
-        params.add(new BasicNameValuePair("client_id", properties.getProperty("yelp.api.client_id")));
-        params.add(new BasicNameValuePair("client_secret", properties.getProperty("yelp.api.client_secret")));
-        post.setEntity(new UrlEncodedFormEntity(params));
+        System.out.printf("Access token:%s%n", token.accessToken());
+        System.out.printf("Token type:%s%n", token.tokenType());
+        System.out.printf("Is it expired? %s%n", token.isExpired());
 
+        // Search restaurants in San Antonio
         CloseableHttpClient client = HttpClientBuilder.create().build();
 
-        CloseableHttpResponse response = client.execute(post);
-
-        HttpEntity entity = response.getEntity();
-        StatusLine line = response.getStatusLine();
-        if (line.getStatusCode() != 200) {
-            System.out.println("something went wrong...");
-            return;
-        }
-        JSONObject token = new JSONObject(EntityUtils.toString(entity));
-
-        String accessToken = token.getString("access_token");
-        System.out.printf("Access token:%s%n", accessToken);
-        System.out.printf("Token type:%s%n", token.getString("token_type"));
-        System.out.printf("Expires in seconds:%s%n", token.getLong("expires_in"));
-
-        // Search in San Antonio
+        URIBuilder builder = new URIBuilder();
         builder
             .setScheme("https")
             .setHost("api.yelp.com")
@@ -67,28 +47,32 @@ public class App
             .setParameter("location", "San Antonio")
         ;
         HttpGet get = new HttpGet(builder.build());
-        get.setHeader("Authorization", String.format("Bearer %s", accessToken));
-        response = client.execute(get);
-        entity = response.getEntity();
-        line = response.getStatusLine();
+        get.setHeader("Authorization", String.format("Bearer %s", token.accessToken()));
+
+        CloseableHttpResponse response = client.execute(get);
+        HttpEntity entity = response.getEntity();
+        StatusLine line = response.getStatusLine();
         if (line.getStatusCode() != 200) {
             System.out.println("Cannot find restaurants in San Antonio...");
             return;
         }
+
         JSONObject restaurants = new JSONObject(EntityUtils.toString(entity));
         JSONArray businesses = restaurants.getJSONArray("businesses");
-        System.out.println(businesses.length());
         JSONObject business = businesses.getJSONObject(0);
+        JSONObject location = business.getJSONObject("location");
+        JSONObject coordinates = business.getJSONObject("coordinates");
+        JSONArray transactions = business.getJSONArray("transactions");
+
+        System.out.println(businesses.length());
         System.out.println(business.getString("name"));
         System.out.println(business.getInt("rating"));
         System.out.println(business.getDouble("distance"));
-        JSONObject location = business.getJSONObject("location");
         System.out.println(location.getString("address1"));
         System.out.println(location.getString("zip_code"));
-        JSONObject coordinates = business.getJSONObject("coordinates");
         System.out.println(coordinates.getDouble("latitude"));
         System.out.println(coordinates.getDouble("longitude"));
-        JSONArray transactions = business.getJSONArray("transactions");
+
         if (transactions.length() > 0) transactions.getString(0);
     }
 }
