f9031e37809724e98344e769061ac9b9fc05c0a4
Luis Montealegre <montealegreluis@gmail.com>
2017-07-15 21:36:01 -0500

    docs: Add LICENSE to the project
    * Add MIT license file

--- src/main/java/com/montealegreluis/yelpv3/App.java
+++ src/main/java/com/montealegreluis/yelpv3/App.java
@@ -1,3 +1,6 @@
+/*
+ * This source file is subject to the license that is bundled with this package in the file LICENSE.
+ */
 package com.montealegreluis.yelpv3;
 
 import org.apache.http.HttpEntity;
