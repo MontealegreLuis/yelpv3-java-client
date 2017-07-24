9c4da879194e14f10f903f1da563b859d5b75ab3
Luis Montealegre <montealegreluis@gmail.com>
2017-07-23 21:19:07 -0500

    feature: Add schedule representation for Business
    * Add Hours class, use Java types for Day and Time instead of strings

---
+++ src/main/java/com/montealegreluis/yelpv3/Hours.java
@@ -0,0 +1,49 @@
+/*
+ * This source file is subject to the license that is bundled with this package in the file LICENSE.
+ */
+package com.montealegreluis.yelpv3;
+
+import org.json.JSONObject;
+
+import java.time.DayOfWeek;
+import java.time.LocalTime;
+
+public class Hours {
+    private boolean isOvernight = false;
+    private DayOfWeek day;
+    private LocalTime start;
+    private LocalTime end;
+
+    public static Hours from(JSONObject jsonHours) {
+        return new Hours(jsonHours);
+    }
+
+    public boolean isOvernight() {
+        return isOvernight;
+    }
+
+    public DayOfWeek day() {
+        return day;
+    }
+
+    public LocalTime start() {
+        return start;
+    }
+
+    public LocalTime end() {
+        return end;
+    }
+
+    private Hours(JSONObject jsonHours) {
+        this.start = createTimeFrom(jsonHours.getString("start"));
+        this.end = createTimeFrom(jsonHours.getString("end"));
+        day = DayOfWeek.of(jsonHours.getInt("day") + 1);
+    }
+
+    private LocalTime createTimeFrom(String text) {
+        return LocalTime.of(
+            Integer.valueOf(text.substring(0, 2)),
+            Integer.valueOf(text.substring(2, 4))
+        );
+    }
+}
--- src/main/java/com/montealegreluis/yelpv3/Schedule.java
+++ src/main/java/com/montealegreluis/yelpv3/Schedule.java
@@ -6,19 +6,37 @@ package com.montealegreluis.yelpv3;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Schedule {
+    private String hoursType = "REGULAR";
     private boolean openNow;
+    private List<Hours> hours = new ArrayList<>();
 
     public static Schedule from(JSONArray hours) {
         return new Schedule(hours);
+    }
+
+    public String hoursType() {
+        return hoursType;
     }
 
     public boolean isOpenNow() {
         return openNow;
     }
 
+    public List<Hours> hours() {
+        return hours;
+    }
+
     private Schedule(JSONArray hours) {
         JSONObject weekSchedule = hours.getJSONObject(0);
         openNow = weekSchedule.getBoolean("is_open_now");
+        setHours(weekSchedule.getJSONArray("open"));
+    }
+
+    private void setHours(JSONArray hours) {
+        for (int i = 0; i < hours.length(); i++) this.hours.add(Hours.from(hours.getJSONObject(i)));
     }
 }
