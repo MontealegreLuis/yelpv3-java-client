8ec2d907204075f056f15cccddca674b68534825
Luis Montealegre <montealegreluis@gmail.com>
2017-07-29 21:25:00 -0500

    refactor: Extract parser for BasicInformation
    * Remove named constructor in BasicInformation class

--- src/main/java/com/montealegreluis/yelpv3/businesses/BasicInformation.java
+++ src/main/java/com/montealegreluis/yelpv3/businesses/BasicInformation.java
@@ -3,10 +3,6 @@
  */
 package com.montealegreluis.yelpv3.businesses;
 
-import org.json.JSONArray;
-import org.json.JSONObject;
-
-import java.util.ArrayList;
 import java.util.List;
 
 public class BasicInformation {
@@ -15,7 +11,7 @@ public class BasicInformation {
     public final String phone;
     public final String id;
     public final boolean closedPermanently;
-    public final List<Category> categories = new ArrayList<>();
+    public final List<Category> categories;
     public final int reviewCount;
     public final String name;
     public final String url;
@@ -23,36 +19,37 @@ public class BasicInformation {
     public final String image;
     public final Location location;
     public final Distance distance;
-    public final List<String> transactions = new ArrayList<>();
+    public final List<String> transactions;
 
-    public static BasicInformation from(JSONObject information) {
-        return new BasicInformation(information);
-    }
-
-    private BasicInformation(JSONObject information) {
-        rating = information.getDouble("rating");
-        pricingLevel = information.has("price") ? PricingLevel.fromSymbol(information.getString("price")) : PricingLevel.NONE;
-        phone = information.getString("phone");
-        id = information.getString("id");
-        closedPermanently = information.getBoolean("is_closed");
-        setCategories(information.getJSONArray("categories"));
-        reviewCount = information.getInt("review_count");
-        name = information.getString("name");
-        url = information.getString("url");
-        coordinates = Coordinates.from(information.getJSONObject("coordinates"));
-        image = information.getString("image_url");
-        location = Location.from(information.getJSONObject("location"));
-        distance = !information.isNull("distance") ? new Distance(information.getDouble("distance")) : null;
-        setTransactions(information.getJSONArray("transactions"));
-    }
-
-    private void setCategories(JSONArray businessCategories) {
-        for (int i = 0; i < businessCategories.length(); i++)
-            categories.add(Category.from(businessCategories.getJSONObject(i)));
-    }
-
-    private void setTransactions(JSONArray transactions) {
-        for (int i = 0; i < transactions.length(); i++)
-            this.transactions.add(transactions.getString(i));
+    public BasicInformation(
+        double rating,
+        PricingLevel pricingLevel,
+        String phone,
+        String id,
+        boolean closedPermanently,
+        List<Category> categories,
+        int reviewCount,
+        String name,
+        String url,
+        Coordinates coordinates,
+        String image,
+        Location location,
+        Distance distance,
+        List<String> transactions
+    ) {
+        this.rating = rating;
+        this.pricingLevel = pricingLevel;
+        this.phone = phone;
+        this.id = id;
+        this.closedPermanently = closedPermanently;
+        this.categories = categories;
+        this.reviewCount = reviewCount;
+        this.name = name;
+        this.url = url;
+        this.coordinates = coordinates;
+        this.image = image;
+        this.location = location;
+        this.distance = distance;
+        this.transactions = transactions;
     }
 }
--- src/main/java/com/montealegreluis/yelpv3/parser/BusinessParser.java
+++ src/main/java/com/montealegreluis/yelpv3/parser/BusinessParser.java
@@ -3,21 +3,58 @@
  */
 package com.montealegreluis.yelpv3.parser;
 
-import com.montealegreluis.yelpv3.businesses.BasicInformation;
-import com.montealegreluis.yelpv3.businesses.Business;
-import com.montealegreluis.yelpv3.businesses.Details;
+import com.montealegreluis.yelpv3.businesses.*;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-public class BusinessParser {
-    public static Business parseFrom(JSONObject businessInformation) {
+import java.util.ArrayList;
+import java.util.List;
+
+class BusinessParser {
+    static Business parseFrom(JSONObject businessInformation) {
         try {
             return new Business(
-                BasicInformation.from(businessInformation),
+                BasicInformationParser.from(businessInformation),
                 Details.from(businessInformation)
             );
         } catch (JSONException exception) {
             throw ParsingFailure.producedBy(businessInformation, exception);
         }
+    }
+}
+
+class BasicInformationParser {
+    static BasicInformation from(JSONObject information) {
+        return new BasicInformation(
+            information.getDouble("rating"),
+            information.has("price") ? PricingLevel.fromSymbol(information.getString("price")) : PricingLevel.NONE,
+            information.getString("phone"),
+            information.getString("id"),
+            information.getBoolean("is_closed"),
+            setCategories(information.getJSONArray("categories")),
+            information.getInt("review_count"),
+            information.getString("name"),
+            information.getString("url"),
+            Coordinates.from(information.getJSONObject("coordinates")),
+            information.getString("image_url"),
+            Location.from(information.getJSONObject("location")),
+            !information.isNull("distance") ? new Distance(information.getDouble("distance")) : null,
+            setTransactions(information.getJSONArray("transactions"))
+        );
+    }
+
+    private static List<Category> setCategories(JSONArray businessCategories) {
+        List<Category> categories = new ArrayList<>();
+        for (int i = 0; i < businessCategories.length(); i++)
+            categories.add(Category.from(businessCategories.getJSONObject(i)));
+        return categories;
+    }
+
+    private static List<String> setTransactions(JSONArray registeredTransactions) {
+        List<String> transactions = new ArrayList<>();
+        for (int i = 0; i < registeredTransactions.length(); i++)
+            transactions.add(registeredTransactions.getString(i));
+        return transactions;
     }
 }
