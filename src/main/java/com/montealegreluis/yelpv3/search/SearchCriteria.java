211c747cf92888798df637fdb56c5b04d5038cf5
Luis Montealegre <montealegreluis@gmail.com>
2017-08-29 09:46:03 -0500

    docs: Add doc blocks to SearchCriteria
    * Add explanation about the optional parameters that criteria can have

--- src/main/java/com/montealegreluis/yelpv3/search/SearchCriteria.java
+++ src/main/java/com/montealegreluis/yelpv3/search/SearchCriteria.java
@@ -18,6 +18,12 @@ public class SearchCriteria {
     private final int defaultPageSize = 20;
     private Map<String, String> parameters = new HashMap<>();
 
+    /**
+     * <code>location</code> is the combination of:
+     * <p>
+     * Address, neighborhood, city, state or zip, optional country to be used when searching for
+     * businesses.
+     */
     public static SearchCriteria byLocation(String location) {
         return new SearchCriteria(location);
     }
@@ -30,24 +36,51 @@ public class SearchCriteria {
         return new SearchCriteria(latitude, longitude);
     }
 
+    /**
+     * Number of business results to return. Default value is 20. Maximum is 50
+     */
     public void limit(Integer limit) {
         if (limit > 50) throw TooManyResults.requested(limit);
 
         parameters.put("limit", limit.toString());
     }
 
+    /**
+     * Maximum offset is 1, 000
+     */
     public void offset(Integer offset) {
         parameters.put("offset", offset.toString());
     }
 
+    /**
+     * Sort the results by one of the values in <code>SortingMode</code>
+     * <p>
+     * Default values is <code>SortingMode.BEST_MATCH</code>.
+     * <p>
+     * The rating sort is not strictly sorted by the rating value, but by an adjusted rating value
+     * that takes into account the number of ratings, similar to a bayesian average. This is so a
+     * business with 1 rating of 5 stars doesn’t immediately jump to the top.
+     */
     public void sortBy(SortingMode mode) {
         parameters.put("sort_by", mode.toString());
     }
 
+    /**
+     * Search term, e.g. "food", "restaurants"
+     * <p>
+     * The term keyword also accepts business names such as "Starbucks".
+     */
     public void withTerm(String term) {
         parameters.put("term", term);
     }
 
+    /**
+     * Search radius in meters or miles.
+     * <p>
+     * The max value is 40000 meters (25 miles).
+     *
+     * @see Distance
+     */
     public void withinARadiusOf(Distance distance) {
         if (distance.biggerThan(Distance.largest())) throw AreaTooLarge.withADistanceOf(distance);
 
@@ -57,6 +90,11 @@ public class SearchCriteria {
         );
     }
 
+    /**
+     * Only return the businesses open now
+     *
+     * @throws IncompatibleCriteria When used in combination with <code>openAt</code>
+     */
     public void openNow() {
         if (parameters.containsKey("open_at"))
             throw IncompatibleCriteria.mixing("open_at", "open_now");
@@ -66,7 +104,8 @@ public class SearchCriteria {
 
     /**
      * @param categories Comma separated list of categories
-     *                   See https://www.yelp.com/developers/documentation/v2/all_category_list
+     * @see SearchCategories
+     * @link https://www.yelp.com/developers/documentation/v2/all_category_list
      */
     public void inCategories(String categories) {
         parameters.put("categories", categories);
@@ -76,12 +115,23 @@ public class SearchCriteria {
         parameters.put("price", level.value().toString());
     }
 
+    /**
+     * If multiple attributes are used, only businesses that satisfy ALL attributes will be returned
+     * in search results.
+     * <p>
+     * For example, the attributes <code>hot_and_new,cashback</code> will return businesses that are
+     * Hot and New AND offer Cash Back.
+     */
     public void withAttributes(Attribute... attributes) {
         StringBuilder filters = new StringBuilder();
         for (Attribute attribute : attributes) filters.append(attribute.value()).append(",");
         parameters.put("attributes", filters.substring(0, filters.length() - 1));
     }
 
+    /**
+     * @throws IncompatibleCriteria When used in combination with <code>openNow</code>
+     * @param timestamp Unix timestamp in the same timezone of the search location
+     */
     public void openAt(Long timestamp) {
         if (parameters.containsKey("open_now"))
             throw IncompatibleCriteria.mixing("open_now", "open_at");
@@ -90,8 +140,9 @@ public class SearchCriteria {
     }
 
     /**
-     * See https://www.yelp.com/developers/documentation/v3/supported_locales for the full list
-     * of supported locales
+     * See the documentation for the full list of supported locales
+     *
+     * @link https://www.yelp.com/developers/documentation/v3/supported_locales
      */
     public void withLocale(Locale locale) {
         parameters.put("locale", locale.toString());
@@ -109,6 +160,9 @@ public class SearchCriteria {
         return QueryString.build(parameters, limit());
     }
 
+    /**
+     * If a limit is not specified it will return the default page size (20)
+     */
     public int limit() {
         return parameters.containsKey("limit")
             ? Integer.valueOf(parameters.get("limit"))
@@ -116,15 +170,14 @@ public class SearchCriteria {
         ;
     }
 
+    /**
+     * If an offset is not specified it will return the default offset (0)
+     */
     public int offset() {
         return parameters.containsKey("offset")
             ? Integer.valueOf(parameters.get("offset"))
             : 0
         ;
-    }
-
-    SearchCriteria(SearchCriteria another) {
-        parameters = new HashMap<>(another.parameters);
     }
 
     private SearchCriteria(Double latitude, Double longitude) {
